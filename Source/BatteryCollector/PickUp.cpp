3e165a21f3af348f585a9cd88939544f63782cd3
Mr. shi <c_shiyibin@qq.com>
2019-01-18 19:00:33 +0800

    original commit

---
+++ Source/BatteryCollector/PickUp.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PickUp.h"
+
+// Sets default values
+APickUp::APickUp()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PickupMesh"));
+	RootComponent = PickupMesh;
+}
+
+// Called when the game starts or when spawned
+void APickUp::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APickUp::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/BatteryCollector/PickUp.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PickUp.generated.h"
+
+UCLASS()
+class BATTERYCOLLECTOR_API APickUp : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickUp();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	FORCEINLINE UStaticMeshComponent* GetMesh() { return PickupMesh; }
+
+	UFUNCTION(BlueprintPure, category = "Pickup")
+	bool IsActive();
+
+	UFUNCTION(BlueprintCallable,category = "Pickup")
+	void SetIsActive(bool NewPickupState);
+
+	
+protected:
+	bool bIsActive;
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, category = "Pickup", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* PickupMesh;
+};
